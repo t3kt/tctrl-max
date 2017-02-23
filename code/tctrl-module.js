1a2aa7600c3af0135f64f6730c0ee0c5c52193be
t3kt <tommy.etkin@gmail.com>
2017-02-22 21:19:14 -0800

    preparing for type-specific control creation

--- code/tctrl-module.js
+++ code/tctrl-module.js
@@ -2,7 +2,7 @@ var START_X = 10;
 var START_Y = 20;
 var TOGGLE_HEIGHT = 25;
 var TOGGLE_WIDTH = 135;
-var PADDING_Y = 5;
+var PADDING_Y = 2;
 var VAR_NAME_PREFIX = 'ctrl__';
 var modSpec;
 var currentPosition;
@@ -17,12 +17,73 @@ function loadModuleSpec(modSpecJson) {
 
   var params = (modSpec && modSpec.params) || [];
   for (var i = 0; i < params.length; i++) {
-    _addParameter(params[i], i);
+    _addParameterNEW(params[i], i);
   }
 }
 
+function TypeHandler(opts) {
+  this.patchFile = opts.patchFile;
+  this.width = opts.width;
+  this.height = opts.height;
+  if (opts.getConfigMessages) {
+    this.getConfigMessages = opts.getConfigMessages;
+  }
+}
+TypeHandler.prototype.build = function(patcher, paramSpec, i, position) {
+  var ctrl = createBpatcher(patcher, this.patchFile, position, VAR_NAME_PREFIX + i, paramSpec.key);
+  ctrl.rect = [
+    ctrl.rect[0],
+    ctrl.rect[1],
+    ctrl.rect[0] + this.width,
+    ctrl.rect[1] + this.height
+  ];
+  // there has to be a better way to do this
+  var modScript = patcher.getnamed('modscript');
+  patcher.connect(modScript, 0, ctrl, 1);
+  this.sendConfigMessages(paramSpec);
+  patcher.disconnect(modScript, 0, ctrl, 1);
+
+  position[1] += this.height + PADDING_Y;
+  return ctrl;
+};
+TypeHandler.prototype.sendConfigMessages = function(paramSpec) {
+  sendConfigMessage(['setlabel', (paramSpec.label || paramSpec.key)]);
+  sendConfigMessage(['setpath', (paramSpec.path || '<none>')]);
+  if (this.getConfigMessages) {
+    var messages = this.getConfigMessages(paramSpec);
+    if (messages) {
+      for (var i = 0; i < messages.length; i++) {
+        sendConfigMessage(messages[i]);
+      }
+    }
+  }
+};
+
+var typeHandlers = {
+  'bool': new TypeHandler({
+    patchFile: 'tctrl-toggle.maxpat',
+    width: 135,
+    height: 25,
+    getConfigMessages: function(paramSpec) {
+      return [
+        ['setdefault', paramSpec['default']]
+      ];
+    }
+  })
+};
+
+function _addParameterNEW(paramSpec, i) {
+  var type = paramSpec.type;
+  var handler = typeHandlers[type];
+  if (!handler) {
+    post('Unsupported parameter type ' + type + ' (' + paramSpec.key + ')\n');
+    return;
+  }
+  handler.build(this.patcher, paramSpec, i, currentPosition);
+}
+
 function _addParameter(paramSpec, i) {
-  var ctrl = createBpatcher(
+  var ctrl = createBpatcher(this.patcher,
     'tctrl-toggle.maxpat',
     currentPosition,
     VAR_NAME_PREFIX + i,
@@ -48,9 +109,9 @@ function _addParameter(paramSpec, i) {
   currentPosition[1] += TOGGLE_HEIGHT + PADDING_Y;
 }
 
-function createBpatcher(file, position,varname,args)
+function createBpatcher(patcher, file, position,varname,args)
 {
-  return this.patcher.newdefault(position[0],position[1],
+  return patcher.newdefault(position[0],position[1],
     "bpatcher",
     "@name", file,
     "@border", "0",
